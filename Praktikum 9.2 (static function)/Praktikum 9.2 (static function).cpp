e235e0ebbe32a5915ef9b76725c056989dca7a8b
gibranrafii <gibranrafi21@gmail.com>
2024-05-14 10:46:24 +0700

    Membuat class mahasiswa dengan beberapa member yang bersifat public dan private

--- Praktikum 9.2 (static function)/Praktikum 9.2 (static function).cpp
+++ Praktikum 9.2 (static function)/Praktikum 9.2 (static function).cpp
@@ -2,6 +2,22 @@
 #include <string>
 using namespace std;
 
+class Mahasiswa {
+private:
+    static int nim;
+public:
+    int id;
+    string nama;
+
+    void setID();
+    void printAll();
+
+    static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
+    static int getNim() { return nim; /*Definisi Function*/ }
+
+    Mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
 
 int main()
 {
