553d207f03789dcf9aa7ec256e720a07db497f29
gibranrafii <gibranrafi21@gmail.com>
2024-05-14 10:21:19 +0700

    Membuat Prosedur printAll untuk menampilkan

--- Praktikum 9.1 (static)/Praktikum 9.1 (static).cpp
+++ Praktikum 9.1 (static)/Praktikum 9.1 (static).cpp
@@ -22,6 +22,11 @@ void Mahasiswa::setID() {
     id = ++nim;
 }
 
+void Mahasiswa::printAll() {
+    cout << "ID     =" << id << endl;
+    cout << "Nama   =" << nama << endl;
+    cout << endl;
+}
 
 int main()
 {
