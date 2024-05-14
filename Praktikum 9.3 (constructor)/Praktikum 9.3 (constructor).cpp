e912ef703c6d1e45438fba96fa17945d35c6ffae
gibranrafii <gibranrafi21@gmail.com>
2024-05-14 11:38:30 +0700

    menyelesaikan program dan revisi pada bagian bagian yang error

--- Praktikum 9.3 (constructor)/Praktikum 9.3 (constructor).cpp
+++ Praktikum 9.3 (constructor)/Praktikum 9.3 (constructor).cpp
@@ -8,14 +8,48 @@ private:
     string nama;
 
 public:
-    Mahasiswa();
-    Mahasiswa(int);
+    Mahasiswa() {
+        nim = 0;
+        nama = "";
+    };
+
+    Mahasiswa(int iNim) {
+        nim = iNim;
+    };
+
     Mahasiswa(string);
     Mahasiswa(int iNim, string iNama);
     void cetak();
 };
 
+Mahasiswa::Mahasiswa(string iNama) {
+    nim = 0;
+    nama = iNama;
+}
+
+Mahasiswa::Mahasiswa(int iNim, string iNama) {
+    nim = iNim;
+    nama = iNama;
+}
+
+void Mahasiswa::cetak() {
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
 int main()
 {
-    
+    Mahasiswa ariel;
+    Mahasiswa duta (223);
+    Mahasiswa apip ("dapa");
+    Mahasiswa apis(123, "faiq");
+
+    ariel.cetak();
+    duta.cetak();
+    apip.cetak();
+    apis.cetak();
+
+    system("pause");
+    return 0;
+
 }
